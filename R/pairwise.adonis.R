5a35cfa116fbf950255045b9f4ad133583e8a819
Julian Trachsel <julestrachsel@gmail.com>
2019-03-07 12:32:58 -0600

    add progress bar

--- R/pairwise.adonis.R
+++ R/pairwise.adonis.R
@@ -66,29 +66,41 @@ pairwise.adonis <- function(x,factors, sim.function = 'vegdist', sim.method = 'b
   F.Model <- c()
   R2 <- c()
   p.value <- c()
+  tot_num_comps <- length(co)/2
+  print(paste('total number of comparisons = ', length(co)/2))
   
+  pb <- txtProgressBar(style = 3)
   
+  current_comp <- 1
   for(elem in 1:ncol(co)){
+    percent_done <- current_comp/tot_num_comps
+    setTxtProgressBar(pb, percent_done)
+    
     if(inherits(x, 'dist')){
       x1=as.matrix(x)[factors %in% c(as.character(co[1,elem]),as.character(co[2,elem])),
                       factors %in% c(as.character(co[1,elem]),as.character(co[2,elem]))]
-      }
-  
+    }
+    
     else  (
       if (sim.function == 'daisy'){
-            x1 = daisy(x[factors %in% c(co[1,elem],co[2,elem]),],metric=sim.method)
-        } 
-      else{x1 = vegdist(x[factors %in% c(co[1,elem],co[2,elem]),],method=sim.method, binary=binary)}
+        x1 = daisy(x[factors %in% c(co[1,elem],co[2,elem]),],metric=sim.method)
+      } 
+      else{
+        
+        x1 = vegdist(x[factors %in% c(co[1,elem],co[2,elem]),],method=sim.method, binary=binary)
+      }
     )
     
     ad <- adonis(x1 ~ factors[factors %in% c(co[1,elem],co[2,elem])],
                  permutations = perm);
     pairs <- c(pairs,paste(co[1,elem],'vs',co[2,elem]));
     Df <- c(Df,ad$aov.tab[1,1])
-	SumsOfSqs <- c(SumsOfSqs, ad$aov.tab[1,2])
-	F.Model <- c(F.Model,ad$aov.tab[1,4]);
+    SumsOfSqs <- c(SumsOfSqs, ad$aov.tab[1,2])
+    F.Model <- c(F.Model,ad$aov.tab[1,4]);
     R2 <- c(R2,ad$aov.tab[1,5]);
     p.value <- c(p.value,ad$aov.tab[1,6])
+    #print(paste('calculating comparison', current_comp))
+    current_comp <- current_comp + 1
   }
   p.adjusted <- p.adjust(p.value,method=p.adjust.m)
   
@@ -104,15 +116,17 @@ pairwise.adonis <- function(x,factors, sim.function = 'vegdist', sim.method = 'b
     pairw.res$p.adjusted <- p.adjust(pairw.res$p.value,method=p.adjust.m)
     
     sig = c(rep('',length(pairw.res$p.adjusted)))
- 	sig[pairw.res$p.adjusted <= 0.1] <-'.'
-	sig[pairw.res$p.adjusted <= 0.05] <-'*'
-	sig[pairw.res$p.adjusted <= 0.01] <-'**'
-	sig[pairw.res$p.adjusted <= 0.001] <-'***'
+    sig[pairw.res$p.adjusted <= 0.1] <-'.'
+    sig[pairw.res$p.adjusted <= 0.05] <-'*'
+    sig[pairw.res$p.adjusted <= 0.01] <-'**'
+    sig[pairw.res$p.adjusted <= 0.001] <-'***'
     pairw.res <- data.frame(pairw.res[,1:7],sig)
   }
   class(pairw.res) <- c("pwadonis", "data.frame")
+  close(pb)
   return(pairw.res)
 } 
+
 
 
 ### Method summary
